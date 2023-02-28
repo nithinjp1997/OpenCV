47fbbae27cbdda092ef6d2081a65e1e9b27b974e
nithinjp1997 <nithinjp@gmail.com>
2023-02-28 17:42:11 +0530

    Corrected Issue with file path , Starting with no has an issue

--- PoseDetection/Code/PoseEstimationMin.py
+++ PoseDetection/Code/PoseEstimationMin.py
@@ -1,26 +1,40 @@
 import cv2 as cv
 import mediapipe as mp
+import time
 
-cap = cv.VideoCapture('PoseDetection\Videos\2.mp4')
+cap = cv.VideoCapture("PoseDetection\Videos\Vid1.mp4")
+
+# h = int(cap.get(cv.CAP_PROP_FOURCC))
+# codec = chr(h&0xff) + chr((h>>8)&0xff) + chr((h>>16)&0xff) + chr((h>>24)&0xff)
+# print(codec)
+
 # Get the video's resolution
 width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
 
 # Set the window size to match the video's aspect ratio
-aspect_ratio = 4/3
-window_height = 500
+aspect_ratio = width / height
+window_height = 600
 window_width = int(window_height * aspect_ratio)
 cv.namedWindow('Image', cv.WINDOW_NORMAL)
 cv.resizeWindow('Image', window_width, window_height)
+pTime = 0
 
-while True:
+while (cap.isOpened()):
     success, img = cap.read()
     if not success:
         # If the video has ended, reset the video to the beginning
         cap.set(cv.CAP_PROP_POS_FRAMES, 0)
         continue
+    
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv.putText(img, str(int(fps)), (70,50), cv.FONT_HERSHEY_PLAIN, 3, (255,0,0), 3)
     cv.imshow("Image", img)
-    cv.waitKey(0)
+    if cv.waitKey(1) == ord('q'):
+        break
 
 cap.release()
 cv.destroyAllWindows()
